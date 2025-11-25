8ecd7a5a7de9c7391a7c9c4871d4890b15facb3a
ruu2023 <kokihiru2038@gmail.com>
2025-11-25 23:51:49 +0900

    user create

---
+++ src/main/java/com/example/firstapp/configs/ApiRoutes.java
@@ -0,0 +1,7 @@
+package com.example.firstapp.configs;
+
+public class ApiRoutes {
+	public static final class User {
+		public static final String CREATE = "/api/v1/user";
+	}
+}
--- src/main/java/com/example/firstapp/controller/FirstController.java
+++ src/main/java/com/example/firstapp/controller/FirstController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.firstapp.configs.ApiRoutes;
 import com.example.firstapp.entity.User;
 import com.example.firstapp.repository.UserRepository;
 
@@ -27,6 +29,14 @@ public class FirstController {
 		String name = userList.get(0).getName();
 		
 		return userList;
+	}
+	
+	@PostMapping(ApiRoutes.User.CREATE)
+	public String create() {
+		User user = new User();
+		user.setName("本日");
+		userRepository.save(user);
+		return "created";
 	}
 
 }
